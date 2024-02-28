e1cbdbcbaa7a6dcbf83586b439a1e07af1e54665
Hassan JERRAR <72023726+Hassan-JERRAR@users.noreply.github.com>
2024-02-28 21:40:54 +0100

    start of product detail page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>Home</h1>
+            <p>Welcome to the home page</p>
+        </div>
+    );
 }
