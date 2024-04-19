76a8b9eeca6f68560befa0ff5244a14d86d97f42
AndreClaveria <andreclaveria2002@gmail.com>
2024-04-19 14:18:42 +0200

    user modify change directly

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -7,113 +7,112 @@ import Edit from "@/components/Edit";
 import Button from "@/components/UI/Button";
 import CardUser from "@/components/UI/card/user_card";
 import FormUser from "@/components/UI/form/user_form";
-import { Toaster, toast } from 'react-hot-toast';
+import { Toaster, toast } from "react-hot-toast";
 
 export default function Users() {
-    const title = "users";
-    const db_name = "users";
-    const add = false;
+  const title = "users";
+  const db_name = "users";
+  const add = false;
 
-    const [dataList, setDataList] = useState([]);
-    const [openForm, setOpenForm] = useState(false);
-    const [selectedData, setSelectedData] = useState(null);
-    const [isEdit, setIsEdit] = useState(false);
+  const [dataList, setDataList] = useState([]);
+  const [openForm, setOpenForm] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const { results, success } = await FetchApi({
-                    url: `/api/${db_name}`,
-                    method: "GET",
-                });
-                if (success) {
-                    setDataList(results);
-                } else {
-                    console.log("fetching data failed");
-                }
-            } catch (error) {
-                console.error("Fetching error: ", error);
-            }
-        };
-
-        fetchData();
-    }, [db_name]);
-
-    const deleteData = async (id) => {
-        const confirmDelete = window.confirm(
-            "Êtes-vous sûr de vouloir supprimer cet élément ?"
-        );
-        if (confirmDelete) {
-            try {
-                await FetchApi({ url: `/api/${db_name}/${id}`, method: "DELETE" });
-                setDataList(dataList.filter((item) => item.id !== id));
-                console.log(`User with ID ${id} deleted successfully`);
-                toast.success(`User deleted successfully`);
-            } 
-            catch (error) {
-                console.error("Deletion error: ", error);
-                console.log(`Failed to delete user with ID ${id}`);
-                toast.error(`Failed to delete user with ID ${id}`);
-            }   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { results, success } = await FetchApi({
+          url: `/api/${db_name}`,
+          method: "GET",
+        });
+        console.log("result : ", results);
+        if (success) {
+          setDataList(results);
+        } else {
+          console.log("fetching data failed");
         }
+      } catch (error) {
+        console.error("Fetching error: ", error);
+      }
     };
 
-    return (
-        <>
-            {openForm && (
-                <Edit
-                    setIsOpen={setOpenForm}
-                    data={selectedData}
-                    edit={isEdit}
-                    FormData={FormUser}
-                    db_name={db_name}
-                    setDataList={setDataList}
-                    dataList={dataList}
-                />
-            )}
-            <Toaster />
+    fetchData();
+  }, [db_name]);
 
-            <div className={styles.listContainer}>
-                <h1 className={styles.listTitle}>{title}</h1>
-                {dataList.map((data) => (
-                    <div
-                        key={data.id}
-                        className={`${styles.list} ${
-                            title === "product" ? styles.small : ""
-                        }`}
-                    >
-                        <CardUser data={data} />
-                        <div className={styles.flexrow}>
-                            <Button
-                                className="red"
-                                clickHandler={() => deleteData(data.id)}
-                                title="delete"
-                            />
-                            <Button
-                                clickHandler={() => {
-                                    setSelectedData(data);
-                                    setOpenForm(true);
-                                    setIsEdit(true);
-                                }}
-                                title="edit"
-                            />
-                            <Link href={`/${db_name}/${data.id}`}>
-                                <Button title="view" />
-                            </Link>
-                        </div>
-                    </div>
-                ))}
-                {add && (
-                    <Button
-                        clickHandler={() => {
-                            setSelectedData(null);
-                            setOpenForm(true);
-                            setIsEdit(false);
-                        }}
-                        title="add"
-                    />
-                )}
-            </div>
-        </>
+  const deleteData = async (id) => {
+    const confirmDelete = window.confirm(
+      "Êtes-vous sûr de vouloir supprimer cet élément ?"
     );
+    if (confirmDelete) {
+      try {
+        await FetchApi({ url: `/api/${db_name}/${id}`, method: "DELETE" });
+        setDataList(dataList.filter((item) => item.id !== id));
+        console.log(`User with ID ${id} deleted successfully`);
+        toast.success(`User deleted successfully`);
+      } catch (error) {
+        console.error("Deletion error: ", error);
+        console.log(`Failed to delete user with ID ${id}`);
+        toast.error(`Failed to delete user with ID ${id}`);
+      }
+    }
+  };
+
+  return (
+    <>
+      {openForm && (
+        <Edit
+          setIsOpen={setOpenForm}
+          data={selectedData}
+          edit={isEdit}
+          FormData={FormUser}
+          db_name={db_name}
+          setDataList={setDataList}
+          dataList={dataList}
+        />
+      )}
+      <Toaster />
+
+      <div className={styles.listContainer}>
+        <h1 className={styles.listTitle}>{title}</h1>
+        {dataList.map((data) => (
+          <div
+            key={data.id}
+            className={`${styles.list} ${
+              title === "product" ? styles.small : ""
+            }`}>
+            <CardUser data={data} />
+            <div className={styles.flexrow}>
+              <Button
+                className="red"
+                clickHandler={() => deleteData(data.id)}
+                title="delete"
+              />
+              <Button
+                clickHandler={() => {
+                  setSelectedData(data);
+                  setOpenForm(true);
+                  setIsEdit(true);
+                }}
+                title="edit"
+              />
+              <Link href={`/${db_name}/${data.id}`}>
+                <Button title="view" />
+              </Link>
+            </div>
+          </div>
+        ))}
+        {add && (
+          <Button
+            clickHandler={() => {
+              setSelectedData(null);
+              setOpenForm(true);
+              setIsEdit(false);
+            }}
+            title="add"
+          />
+        )}
+      </div>
+    </>
+  );
 }
--- src/components/Edit/index.js
+++ src/components/Edit/index.js
@@ -2,136 +2,121 @@ import Modal from "@/components/UI/Modal";
 import Loading from "@/components/UI/Loading";
 import Button from "@/components/UI/Button";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import FetchApi from "../useFetch";
-import { toast } from 'react-hot-toast';
+import { toast } from "react-hot-toast";
 
 const Index = ({
-    setIsOpen,
-    data,
-    FormData,
-    db_name,
-    dataList,
-    setDataList,
+  setIsOpen,
+  data,
+  FormData,
+  db_name,
+  dataList,
+  setDataList,
 }) => {
-    const [dataForm, setDataForm] = useState();
-    const [edit, setEdit] = useState(false);
+  const [dataForm, setDataForm] = useState();
+  const [edit, setEdit] = useState(false);
 
-    // handle change input
-    const handleChange = (e) => {
-        setDataForm({ ...dataForm, [e.target.name]: e.target.value });
+  // handle change input
+  const handleChange = (e) => {
+    setDataForm({ ...dataForm, [e.target.name]: e.target.value });
+  };
+
+  // handle image input
+  const handleImage = async (e) => {
+    if (e?.target?.files[0] == undefined || e?.target?.files[0] == null)
+      return console.log("no image");
+
+    // preview image
+    const preview_url = URL.createObjectURL(e.target.files[0]);
+    // convert image to base64 and set it to dataForm state
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = async () => {
+      const base64 = reader.result;
+      setDataForm({
+        ...dataForm,
+        [e.target.name + "_name"]: e.target.files[0].name,
+        [e.target.name + "_base64"]: base64,
+        [e.target.name]: preview_url,
+      });
     };
+  };
 
-    // handle image input
-    const handleImage = async (e) => {
-        if (e?.target?.files[0] == undefined || e?.target?.files[0] == null)
-            return console.log("no image");
+  // delete element (image, file, video, etc...)
+  const deleteElement = async (name) => {
+    setDataForm({ ...dataForm, [name]: "" });
+  };
 
-        // preview image
-        const preview_url = URL.createObjectURL(e.target.files[0]);
-        // convert image to base64 and set it to dataForm state
-        const reader = new FileReader();
-        reader.readAsDataURL(e.target.files[0]);
-        reader.onload = async () => {
-            const base64 = reader.result;
-            setDataForm({
-                ...dataForm,
-                [e.target.name + "_name"]: e.target.files[0].name,
-                [e.target.name + "_base64"]: base64,
-                [e.target.name]: preview_url,
-            });
-        };
-    };
+  useEffect(() => {
+    if (data != undefined) {
+      setDataForm(data);
+      setEdit(true);
+    }
+  }, [data]);
 
-    // delete element (image, file, video, etc...)
-    const deleteElement = async (name) => {
-        setDataForm({ ...dataForm, [name]: "" });
-    };
+  // submit form (edit or add data)
+  const submitForm = async (e) => {
+    e.preventDefault();
+    console.log(dataForm.id);
+    if (edit) {
+      const result = FetchApi({
+        url: `/api/${db_name}/${dataForm.id}`,
+        method: "PUT",
+        body: dataForm,
+      })
+        .then((response) => {
+          setDataList(
+            dataList.map((data) =>
+              data.id == response.results.id ? response.results : data
+            )
+          );
 
-    useEffect(() => {
-        if (data != undefined) {
-            setDataForm(data);
-            setEdit(true);
-        }
-    }, [data]);
+          setIsOpen(false);
+          toast.success("Vos modifications ont été envoyées avec succès !");
+        })
+        .catch((error) => {
+          console.log("error : ", error);
+          toast.error("Une erreur s'est produite. Veuillez réessayer.");
+        });
+    } else {
+      FetchApi({
+        url: `/api/${db_name}`,
+        method: "POST",
+        body: dataForm,
+      })
+        .then((response) => {
+          setDataList([...dataList, response.results]);
+          setIsOpen(false);
+          toast.success("Votre creation a été envoyée avec succès !");
+        })
 
-    // submit form (edit or add data)
-    const submitForm = async (e) => {
-        e.preventDefault();
-        if (edit) {
-            // run edit request
-            const result = FetchApi({
-                url: `/api/${db_name}/${dataForm.id}`,
-                method: "PUT",
-                body: dataForm,
-            })
-                .then((response) => {
-                    setDataList(
-                        dataList.map((data) =>
-                            data.id == response.results.id
-                                ? response.results
-                                : data
-                        )
-                        
-                    );
-                    setIsOpen(false);
-                    toast.success('Vos modifications ont été envoyées avec succès !');
-                })
-                .catch((error) => {
-                    console.log("error : ", error);
-                    toast.error("Une erreur s'est produite. Veuillez réessayer.");
-                });
-        } else {
-            // run add request
-            FetchApi({
-                url: `/api/${db_name}`,
-                method: "POST",
-                body: dataForm,
-            })
-                .then((response) => {
-                    setDataList([...dataList, response.results]);
-                    setIsOpen(false);
-                    toast.success('Votre creation a été envoyée avec succès !');
-                })
-                .catch((error) => {
-                    console.log("error : ", error);
-                });
-        }
-    };
+        .catch((error) => {
+          console.log("error : ", error);
+        });
+    }
+  };
 
-    // modal component
-    return (
-        <Modal
-            title={edit ? "Modify" : "Add"}
-            closeModal={() => setIsOpen(false)}
-        >
-            <form
-                onSubmit={(e) => {
-                    submitForm(e);
-                }}
-            >
-                {/* form data initiated in props */}
-                <FormData
-                    dataForm={dataForm}
-                    handleChange={handleChange}
-                    handleImage={handleImage}
-                    deleteElement={deleteElement}
-                />
-                {(edit && (
-                    <Button
-                        type="submit"
-                        className="btn__primary"
-                        title="Modify"
-                    />
-                )) || (
-                    <Button
-                        type="submit"
-                        className="btn__primary"
-                        title="Add"
-                    />
-                )}
-            </form>
-        </Modal>
-    );
+  // modal component
+  return (
+    <Modal title={edit ? "Modify" : "Add"} closeModal={() => setIsOpen(false)}>
+      <form
+        onSubmit={(e) => {
+          submitForm(e);
+        }}>
+        {/* form data initiated in props */}
+        <FormData
+          dataForm={dataForm}
+          handleChange={handleChange}
+          handleImage={handleImage}
+          deleteElement={deleteElement}
+        />
+        {(edit && (
+          <Button type="submit" className="btn__primary" title="Modify" />
+        )) || <Button type="submit" className="btn__primary" title="Add" />}
+      </form>
+    </Modal>
+  );
 };
 
 export default Index;
