901b5379d7e6aaf5cd40b9d1ea52340c188d7f87
Hassan JERRAR <72023726+Hassan-JERRAR@users.noreply.github.com>
2024-04-19 09:41:53 +0200

    fix token check

--- src/middleware.js
+++ src/middleware.js
@@ -1,26 +1,41 @@
 import { NextResponse } from "next/server";
-import { getCookie } from "cookies-next";
+import { deleteCookie, getCookie } from "cookies-next";
 
-// This function can be marked `async` if using `await` inside,
-// but in this case, it's not necessary since you're not using any asynchronous operations.
-export function middleware(request) {
-    // You correctly check for the cookie in the request.
-    // However, to improve clarity and maintainability, you could assign the result to a variable.
+export async function middleware(request) {
     const token = getCookie("token", { req: request });
 
-    // Use the token to decide whether to redirect or proceed.
     if (!token) {
-        // Ensure that the URL redirection is constructed correctly.
-        // The use of new URL() here is correct and a good practice for constructing absolute URLs.
-        return NextResponse.redirect(new URL("/login", request.url));
+        return NextResponse.redirect(new URL("/login", request.url).toString());
     }
 
-    // Proceed with the request if the token exists.
-    return NextResponse.next();
+    try {
+        const session = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/auth/session`,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                },
+            }
+        );
+
+        if (session.status !== 200) {
+            return NextResponse.redirect(
+                new URL("/login", request.url).toString()
+            );
+        }
+
+        // Si le token est valide, continuer avec la requête
+        return NextResponse.next();
+    } catch (error) {
+        console.error("Error fetching: ", error);
+        deleteCookie("token", { req: request });
+
+        // Rediriger vers la page de connexion en cas d'erreur
+        return NextResponse.redirect(new URL("/login", request.url).toString());
+    }
 }
 
-// Your config object is correct. It specifies which paths the middleware should apply to.
-// Including both "/products" and "/products/:path*" is a good practice to cover the base path and any subpaths.
 export const config = {
     matcher: [
         "/products/:path*",
